90501e2ee5025e2687efc2a4f77404705899dbab
Igors Šaters <s21sateigor@venta.lv>
2023-03-13 10:39:16 +0200

    Add: New blocks

--- src/dataStr/MyLinkedList.java
+++ src/dataStr/MyLinkedList.java
@@ -20,5 +20,47 @@ public class MyLinkedList<T>
             elementCounter++;
         }
     }
+    public void add (T newElement, int position) throws Exception{
+        //TODO verify if not full
+
+        if(position < 0 || position > elementCounter){
+            throw ( new Exception("Wrong position"));
+        }
+
+        //add at the beginning
+        if(position == 0){
+            MyNode<T> newNode = new MyNode<T>(newElement);
+            first.setPrevious(newNode);
+            newNode.setNext(first);
+            first = newNode;
+            elementCounter++;
+        }
+
+        //add at the ending
+
+        else if(position == elementCounter){
+            add(newElement);
+        }
+
+        //add in the middle
+
+        else {
+            MyNode tempNode = first;
+            for(int i = 0; i < position - 1; i++){
+                tempNode = tempNode.getNext();
+            }
+
+            MyNode tempNode2 = tempNode.getNext();
+            MyNode<T> newNode = new MyNode<T>(newElement);
+
+            tempNode.setNext(newNode);
+            newNode.setPrevious(tempNode);
+
+            tempNode2.setPrevious(newNode);
+            newNode.setNext(tempNode2);
+
+            elementCounter++;
+        }
+    }
 
 }
