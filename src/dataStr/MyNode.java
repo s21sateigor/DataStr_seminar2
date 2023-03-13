4a6f79fa93635711d7397636bc0ecf6623df1350
Igors Šaters <s21sateigor@venta.lv>
2023-03-13 09:42:05 +0200

    Add: New class for Node implementation as MyNode

---
+++ src/dataStr/MyNode.java
@@ -0,0 +1,45 @@
+package dataStr;
+
+public class MyNode<T> {
+    private T element;
+    private MyNode next = null;
+    private MyNode previous = null;
+
+
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T inputElement) {
+        if(inputElement != null){
+            element = inputElement;
+        } else {
+            element = (T) new Object();
+        }
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public MyNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(MyNode previous) {
+        this.previous = previous;
+    }
+
+    public MyNode(T inputElement){
+        setElement(inputElement);
+    }
+
+    public String toString(){
+        return "" + element;
+    }
+}
