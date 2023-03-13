8990491c45d7cd736e631211d5698f60e82bea7c
Igors Šaters <s21sateigor@venta.lv>
2023-03-13 11:02:59 +0200

    Add: MainService

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,8 +1,23 @@
 package service;
 
+import dataStr.MyLinkedList;
+
 public class MainService
 {
     public static void main(String[] args) {
+        try {
 
+            MyLinkedList<String> stringList = new MyLinkedList<>();
+            stringList.add("Igors");
+            stringList.add("Jānis");
+            stringList.print();
+            stringList.add("Jana", 0);
+            stringList.print();
+            stringList.add("Līga", 2);
+            stringList.print();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
